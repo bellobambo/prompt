4faec90bcf14e94dd2ddcd4f60e188d3ef74fd8f
bellobambo <bellobambo21@gmail.com>
2023-05-28 19:33:32 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    resolve: {
+        fallback: {
+            util: require.resolve("util/")
+        }
+    }
+
+}
 
 module.exports = nextConfig
